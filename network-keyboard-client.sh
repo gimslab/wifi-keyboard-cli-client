1d719da83b8fc12916a22c8281c2f9a2c8131d35
gimslab.com <gimslab.com@gmail.com>
2021-10-30 17:47:31 +0900

    get first seq, BS, Enter

--- network-keyboard-client.sh
+++ network-keyboard-client.sh
@@ -1,30 +1,50 @@
-#addr='http://192.168.50.220:7777/key?'
-addr='http://192.168.50.220:5555/key_event'
+#!/bin/bash
 
+# exam
+# curl 'http://192.168.50.220:7777/key?215,C97,' \
+# C: character, D: down, U: up
+
+function findStartSeq {
+  curl -s $addr | grep seqConfirmed | head -1 | sed 's/[^0-9]//g'
+}
+
+function sendKey {
+  seq=$1
+  keyCodeParam=$2
+  curl -s ${addr}/key?${seq},$keyCodeParam,
+}
+
+addr='http://192.168.50.220:7777'
+
+mode=$1 # -d debug, -s silent
+debugMode=$( [[ "$mode" = "-d" ]] && echo true || echo false)
+silentMode=$( [[ "$mode" = "-s" ]] && echo true || echo false)
+echo addr: $addr
+echo debugMode: $debugMode
+echo silentMode: $silentMode
+seq=$(findStartSeq)
 
 settings=$(stty -g)
-stty -echo
-#stty stop undef    # or 'stty raw' to read other control chars
+[[ $silentMode = true ]] && stty -echo
 stty raw
 IFS=
 
 while ( true ); do
   read -r -n1 ch
-  echo read=[$ch]
+  [[ $debugMode = true ]] && printf "read:%s\r\n" $ch
   keycode=$(printf "%d" \'$ch\')
-  keycodeHex=$(printf "%x" \'$ch\')
-  echo keycode=[$keycode] [$keycodeHex]
-  databody=$keycode
+  [[ $debugMode = true ]] && printf "keycode: %s\r\n" $keycode
+  keyCodeParam=C$keycode
   
-  [[ $keycode = 127 ]] && databody=-5 # BS
-  [[ $keycode = 39 ]] && databody=10 # Enter
-  
-  echo databobdy=[$databody]
+  [[ $keycode = 127 ]] && keyCodeParam=D8 # BS
+  [[ $keycode = 39 ]] && keyCodeParam=D13 # Enter
+  [[ $keycode = 13 ]] && keyCodeParam=D13 # Enter
 
-  curl $addr --data-raw $databody
+  seq=$(( seq + 1 ))
 
+  response=$(sendKey $seq $keyCodeParam)
+  [[ $debugMode = true ]] && printf "response: %s" $response
 done
-
 
 stty "$settings"
 
